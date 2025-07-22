853b7648fbe1c6cf591f07308b235977996b9d5f
Sudhir Ranjan <160086220+sudhirslab@users.noreply.github.com>
2025-07-21 17:12:26 -0700

    Attachment store for holding the attachments

---
+++ data/attachment_store.py
@@ -0,0 +1,3 @@
+# TODO: This is a temporary store for attachments. We will replace this with a database in the future.
+# TODO: We will also add a UUID for the attachment IDs.
+attachments = []
---
+++ data/case_store.py
@@ -0,0 +1,3 @@
+# TODO: This is a temporary store for cases. We will replace this with a database in the future.
+# TODO: We will also add a UUID for the case IDs.
+cases = []
